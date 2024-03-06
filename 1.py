5739d125097bfcaa4bd2c59d09c127fc5a6abeb9
Sofia <Sonya_yarmolash@mail.ru>
2024-03-07 01:58:45 +0300

    .

---
+++ 1.py
@@ -0,0 +1,6 @@
+c = int(input("Введите n кол-во слов:"))
+a = ""
+for i in range(c):
+    d = input()
+    a = a + d + " "
+print(a)
---
+++ 4.py
@@ -0,0 +1,19 @@
+import random
+schet1 = 0
+schet2 = 0
+kx = 3
+v = ["a", "b"]
+while schet2 != 3:
+    a = random.randint(1, 100)
+    b = random.randint(1,100)
+    print(a, "+", b, "=")
+    sum = a + b
+    otv = int(input("Введите Ваш ответ"))
+    if sum != otv:
+        print("Ответ неверный")
+        schet2 += 1
+    else:
+        print("Правильно")
+        schet1 += 1
+print("Игра окончена. Правильных ответов:", schet1)
+
